250097b0de910a2f2bf2c3b184f866a32ee9e81c
msvalandro <msvalandro@proton.me>
2024-07-21 21:03:22 +0100

    feat: create loading component

--- App.tsx
+++ App.tsx
@@ -1,35 +1,23 @@
+import { Loading } from '@components/Loading'
 import {
   Roboto_400Regular,
   Roboto_700Bold,
   useFonts,
 } from '@expo-google-fonts/roboto'
 import { NativeBaseProvider } from 'native-base'
-import { StatusBar, Text, View } from 'react-native'
+import { StatusBar, View } from 'react-native'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
     <NativeBaseProvider>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: '#202024',
-        }}
-      >
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor="transparent"
-          translucent
-        />
-        {fontsLoaded ? (
-          <Text style={{ color: '#FFF' }}>hello world</Text>
-        ) : (
-          <Text>loading...</Text>
-        )}
-      </View>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {!fontsLoaded ? <View /> : <Loading />}
     </NativeBaseProvider>
   )
 }
