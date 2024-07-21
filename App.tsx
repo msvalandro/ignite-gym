3813e140e059d8bafad0a17f8c8f1926f7dc6f4d
msvalandro <msvalandro@proton.me>
2024-07-21 20:29:27 +0100

    chore: setup native-base lib

--- App.tsx
+++ App.tsx
@@ -3,30 +3,33 @@ import {
   Roboto_700Bold,
   useFonts,
 } from '@expo-google-fonts/roboto'
+import { NativeBaseProvider } from 'native-base'
 import { StatusBar, Text, View } from 'react-native'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#202024',
-      }}
-    >
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? (
-        <Text style={{ color: '#FFF' }}>hello world</Text>
-      ) : (
-        <Text>loading...</Text>
-      )}
-    </View>
+    <NativeBaseProvider>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#202024',
+        }}
+      >
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? (
+          <Text style={{ color: '#FFF' }}>hello world</Text>
+        ) : (
+          <Text>loading...</Text>
+        )}
+      </View>
+    </NativeBaseProvider>
   )
 }
