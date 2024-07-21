fc05b30a2f6fcd4a87005eaa9f323ef8feb8aa53
msvalandro <msvalandro@proton.me>
2024-07-21 21:50:32 +0100

    feat: add outline variant to button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,19 +4,25 @@ interface ButtonProps extends IButtonProps {
   title: string
 }
 
-export function Button({ title, ...rest }: ButtonProps) {
+export function Button({ title, variant, ...rest }: ButtonProps) {
   return (
     <NativeBaseButton
       w="full"
       h={14}
-      bg="green.700"
+      bg={variant === 'outline' ? 'transparent' : 'green.700'}
+      borderWidth={variant === 'outline' ? 1 : 0}
+      borderColor="green.500"
       rounded="sm"
       _pressed={{
-        bg: 'green.500',
+        bg: variant === 'outline' ? 'gray.500' : 'green.500',
       }}
       {...rest}
     >
-      <Text fontFamily="heading" fontSize="sm" color="white">
+      <Text
+        fontFamily="heading"
+        fontSize="sm"
+        color={variant === 'outline' ? 'green.500' : 'white'}
+      >
         {title}
       </Text>
     </NativeBaseButton>
