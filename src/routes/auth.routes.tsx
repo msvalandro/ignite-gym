cc2ca5d6ad1812277ae85fb93d169e1906bb0b0c
msvalandro <msvalandro@proton.me>
2024-07-22 10:34:27 +0100

    refactor: add types to auth routes

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,14 +1,24 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from '@react-navigation/native-stack'
 import { SignIn } from '@screens/SignIn'
 import { SignUp } from '@screens/SignUp'
 
-const { Navigator, Screen } = createNativeStackNavigator()
+type AuthRoutes = {
+  signIn: undefined
+  signUp: undefined
+}
+
+export type AuthNavigatorRoutesProps = NativeStackNavigationProp<AuthRoutes>
+
+const { Navigator, Screen } = createNativeStackNavigator<AuthRoutes>()
 
 export function AuthRoutes() {
   return (
     <Navigator>
-      <Screen name="signin" component={SignIn} />
-      <Screen name="signup" component={SignUp} />
+      <Screen name="signIn" component={SignIn} />
+      <Screen name="signUp" component={SignUp} />
     </Navigator>
   )
 }
