8f447970d210e7997fa07ae3212142d0e55a7557
msvalandro <msvalandro@proton.me>
2024-07-22 19:44:17 +0100

    feat: style home header component

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,11 +2,13 @@ import { Heading, HStack, Text, VStack } from 'native-base'
 
 export function HomeHeader() {
   return (
-    <HStack>
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <VStack>
-        <Text color="gray.100">Olá,</Text>
+        <Text color="gray.100" fontSize="md">
+          Olá,
+        </Text>
 
-        <Heading color="gray.100" fontFamily="heading">
+        <Heading color="gray.100" fontFamily="heading" fontSize="md">
           Matheus Valandro
         </Heading>
       </VStack>
