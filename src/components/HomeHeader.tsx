480441ad232d611ee2fa28d4518a23f1e90b3d3c
msvalandro <msvalandro@proton.me>
2024-07-22 19:42:26 +0100

    feat: create home header component

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,15 @@
+import { Heading, HStack, Text, VStack } from 'native-base'
+
+export function HomeHeader() {
+  return (
+    <HStack>
+      <VStack>
+        <Text color="gray.100">Olá,</Text>
+
+        <Heading color="gray.100" fontFamily="heading">
+          Matheus Valandro
+        </Heading>
+      </VStack>
+    </HStack>
+  )
+}
