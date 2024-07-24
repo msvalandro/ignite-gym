e770dcde42ffcf71042e4ed5c1e5a7c3fcaf0c3c
msvalandro <msvalandro@proton.me>
2024-07-24 09:19:25 +0100

    feat: add skeleton effect to user photo

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,18 +1,33 @@
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
-import { Center, ScrollView, VStack } from 'native-base'
+import { Center, ScrollView, Skeleton, VStack } from 'native-base'
+import { useState } from 'react'
+
+const PHOTO_SIZE = 33
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false)
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
 
       <ScrollView>
         <Center mt={6} px={10}>
-          <UserPhoto
-            source={{ uri: 'https://github.com/msvalandro.png' }}
-            size={20}
-          />
+          {photoIsLoading ? (
+            <Skeleton
+              h={PHOTO_SIZE}
+              w={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: 'https://github.com/msvalandro.png' }}
+              size={PHOTO_SIZE}
+            />
+          )}
         </Center>
       </ScrollView>
     </VStack>
