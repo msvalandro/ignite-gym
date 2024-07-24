b1acced28b83adf74680a333c6dd2c2251520a9f
msvalandro <msvalandro@proton.me>
2024-07-24 17:15:00 +0100

    feat: finish exercise screen header component

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,6 +1,7 @@
+import BodySvg from '@assets/body.svg'
 import { Feather } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
-import { Icon, VStack } from 'native-base'
+import { Heading, HStack, Icon, Text, VStack } from 'native-base'
 import { TouchableOpacity } from 'react-native'
 
 export function Exercise() {
@@ -16,6 +17,30 @@ export function Exercise() {
         <TouchableOpacity onPress={handleGoBack}>
           <Icon as={Feather} name="arrow-left" color="green.500" size={6} />
         </TouchableOpacity>
+
+        <HStack
+          justifyContent="space-between"
+          alignItems="center"
+          mt={4}
+          mb={8}
+        >
+          <Heading
+            fontFamily="heading"
+            fontSize="lg"
+            color="gray.100"
+            flexShrink={1}
+          >
+            Puxada frontal
+          </Heading>
+
+          <HStack alignItems="center">
+            <BodySvg />
+
+            <Text color="gray.200" textTransform="capitalize" ml={1}>
+              Costas
+            </Text>
+          </HStack>
+        </HStack>
       </VStack>
     </VStack>
   )
