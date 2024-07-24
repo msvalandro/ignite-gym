76994ed624c9ac5593dd2ea429ca7f34daed1eaa
msvalandro <msvalandro@proton.me>
2024-07-24 09:20:42 +0100

    feat: add button to change user photo

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,7 +1,8 @@
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
-import { Center, ScrollView, Skeleton, VStack } from 'native-base'
+import { Center, ScrollView, Skeleton, Text, VStack } from 'native-base'
 import { useState } from 'react'
+import { TouchableOpacity } from 'react-native'
 
 const PHOTO_SIZE = 33
 
@@ -28,6 +29,18 @@ export function Profile() {
               size={PHOTO_SIZE}
             />
           )}
+
+          <TouchableOpacity>
+            <Text
+              fontSize="md"
+              fontWeight="bold"
+              color="green.500"
+              mt={2}
+              mb={8}
+            >
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
         </Center>
       </ScrollView>
     </VStack>
