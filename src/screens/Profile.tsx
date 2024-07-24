0c50998f034e92e47bac9856bbaa4bf3c460253c
msvalandro <msvalandro@proton.me>
2024-07-24 17:44:38 +0100

    feat: add logic to access photo lib

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -2,6 +2,7 @@ import { Button } from '@components/Button'
 import { Input } from '@components/Input'
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
+import * as ImagePicker from 'expo-image-picker'
 import {
   Center,
   Heading,
@@ -17,6 +18,10 @@ const PHOTO_SIZE = 33
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false)
+
+  async function handleUserPhotoSelect() {
+    await ImagePicker.launchImageLibraryAsync()
+  }
 
   return (
     <VStack flex={1}>
@@ -39,7 +44,7 @@ export function Profile() {
             />
           )}
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               fontSize="md"
               fontWeight="bold"
