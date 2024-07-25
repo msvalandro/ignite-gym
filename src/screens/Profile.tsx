c1ec71ec965327219e904d6e25e8de0380444525
msvalandro <msvalandro@proton.me>
2024-07-25 17:20:53 +0100

    feat: show toast when image bigger than 5mb

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -10,6 +10,7 @@ import {
   ScrollView,
   Skeleton,
   Text,
+  useToast,
   VStack,
 } from 'native-base'
 import { useState } from 'react'
@@ -22,6 +23,8 @@ export function Profile() {
   const [userPhoto, setUserPhoto] = useState(
     'https://github.com/msvalandro.png',
   )
+
+  const toast = useToast()
 
   async function handleUserPhotoSelect() {
     setPhotoIsLoading(true)
@@ -47,9 +50,11 @@ export function Profile() {
       const photoInfo = await FileSystem.getInfoAsync(photoSelectedUri)
 
       if (photoInfo.exists && photoInfo.size / 1024 / 1024 > 5) {
-        return Alert.alert(
-          'Essa imagem é muito grande. Escolha uma de até 5MB.',
-        )
+        return toast.show({
+          title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
+          placement: 'top',
+          bg: 'red.500',
+        })
       }
 
       setUserPhoto(photoSelectedUri)
