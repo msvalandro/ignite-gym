7f291a96edcf11f28e78dea73a244e4db1b07887
msvalandro <msvalandro@proton.me>
2024-07-26 09:57:06 +0100

    feat: style invalid input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,24 +1,46 @@
-import { IInputProps, Input as NativeBaseInput } from 'native-base'
+import { FormControl, IInputProps, Input as NativeBaseInput } from 'native-base'
 
-export function Input(props: IInputProps) {
+interface InputProps extends IInputProps {
+  errorMessage?: string
+  isInvalid?: boolean
+}
+
+export function Input({
+  errorMessage = '',
+  isInvalid = false,
+  ...rest
+}: InputProps) {
+  const invalid = !!errorMessage || isInvalid
+
   return (
-    <NativeBaseInput
-      bg="gray.700"
-      h={14}
-      px={4}
-      mb={4}
-      borderWidth={1}
-      borderColor="transparent"
-      fontFamily="body"
-      fontSize="md"
-      color="white"
-      placeholderTextColor="gray.300"
-      _focus={{
-        bg: 'gray.700',
-        borderWidth: 1,
-        borderColor: 'green.500',
-      }}
-      {...props}
-    />
+    <FormControl isInvalid={invalid} w="full" mb={4}>
+      <NativeBaseInput
+        bg="gray.700"
+        h={14}
+        px={4}
+        borderWidth={1}
+        borderColor="transparent"
+        fontFamily="body"
+        fontSize="md"
+        color="white"
+        placeholderTextColor="gray.300"
+        isInvalid={invalid}
+        _invalid={{
+          borderWidth: 1,
+          borderColor: 'red.500',
+        }}
+        invalidOutlineColor="red.500"
+        _focus={{
+          bg: 'gray.700',
+          borderWidth: 1,
+          borderColor: 'green.500',
+        }}
+        {...rest}
+      />
+
+      <FormControl.ErrorMessage color="red.500">
+        {errorMessage}
+      </FormControl.ErrorMessage>
+    </FormControl>
   )
 }
