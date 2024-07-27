cd0b5e7d43ac7628ea6fddc346e55080cf4bd87d
msvalandro <msvalandro@proton.me>
2024-07-27 18:25:42 +0100

    refactor: create AppError class

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,4 +4,13 @@ const api = axios.create({
   baseURL: 'http://10.52.197.122:3333',
 })
 
+api.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    return Promise.reject(error)
+  },
+)
+
 export { api }
