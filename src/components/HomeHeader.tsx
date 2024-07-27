8823684606b5d2159b115823a7284379e13ff01e
msvalandro <msvalandro@proton.me>
2024-07-27 19:47:37 +0100

    feat: show default user img when avatar is null

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,3 +1,4 @@
+import DefaultUserPhotoImg from '@assets/userPhotoDefault.png'
 import { MaterialIcons } from '@expo/vector-icons'
 import { useAuth } from '@hooks/useAuth'
 import { Heading, HStack, Icon, Text, VStack } from 'native-base'
@@ -11,7 +12,7 @@ export function HomeHeader() {
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <UserPhoto
-        source={{ uri: 'https://github.com/msvalandro.png' }}
+        source={user.avatar ? { uri: user.avatar } : DefaultUserPhotoImg}
         size={16}
         mr={4}
       />
