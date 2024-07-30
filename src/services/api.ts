d366dc86a85cae8f6ff8473bf4837d025b597fc4
msvalandro <msvalandro@proton.me>
2024-07-30 19:22:05 +0100

    refactor: add failed requests on queue

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,13 @@
 import { storageAuthTokenGet } from '@storage/storageAuthToken'
 import { AppError } from '@utils/AppError'
-import axios, { AxiosInstance } from 'axios'
+import axios, { AxiosError, AxiosInstance } from 'axios'
 
 type SignOut = () => void
+
+interface PromiseType {
+  onSuccess: (token: string) => void
+  onFailure: (error: AxiosError) => void
+}
 
 interface APIInstanceProps extends AxiosInstance {
   registerInterceptTokenManager: (signOut: SignOut) => () => void
@@ -11,6 +16,9 @@ interface APIInstanceProps extends AxiosInstance {
 const api = axios.create({
   baseURL: 'http://10.52.197.122:3333',
 }) as APIInstanceProps
+
+const failedQueue: PromiseType[] = []
+let isRefreshing = false
 
 api.registerInterceptTokenManager = (signOut) => {
   const interceptTokenManager = api.interceptors.response.use(
@@ -27,6 +35,26 @@ api.registerInterceptTokenManager = (signOut) => {
             signOut()
             return Promise.reject(requestError)
           }
+
+          const originalRequestConfig = requestError.config
+
+          if (isRefreshing) {
+            return new Promise((resolve, reject) => {
+              failedQueue.push({
+                onSuccess: (token: string) => {
+                  originalRequestConfig.headers = {
+                    Authorization: `Bearer ${token}`,
+                  }
+                  resolve(api(originalRequestConfig))
+                },
+                onFailure: (error: AxiosError) => {
+                  reject(error)
+                },
+              })
+            })
+          }
+
+          isRefreshing = true
         }
 
         signOut()
